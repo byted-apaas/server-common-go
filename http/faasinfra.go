2fe959c64b5cb831aac66aac4c7557ea06208f3a
shaozhipeng <shaozhipeng@bytedance.com>
2023-09-19 12:53:54 +0800

    support error code
    Change-Id: I12487ea62e9d41e00bc5a2a8e524519a45e4cfd0

--- http/faasinfra.go
+++ http/faasinfra.go
@@ -5,22 +5,14 @@ package http
 
 import (
 	"context"
+	"github.com/byted-apaas/server-common-go/utils"
 
 	exp "github.com/byted-apaas/server-common-go/exceptions"
 	"github.com/tidwall/gjson"
 )
 
-const (
-	SuccessCode                     = "0"
-	FaaSInfraFailCodeInternalError  = "k_ec_000001"
-	FaaSInfraFailCodeTokenExpire    = "k_ident_013000"
-	FaaSInfraFailCodeIllegalToken   = "k_ident_013001"
-	FaaSInfraFailCodeMissingToken   = "k_fs_ec_100001"
-	FaaSInfraFailCodeRateLimitError = "k_fs_ec_000004"
-)
-
 func SendLog(ctx context.Context, data interface{}) error {
-	body, _, err := GetFaaSInfraClient().PostJson(ctx, GetFaaSInfraPathSendLog(), map[string][]string{
+	body, extra, err := GetFaaSInfraClient().PostJson(ctx, GetFaaSInfraPathSendLog(), map[string][]string{
 		"Kldx-Version": {"4.0.0"}, // TODO FaaSInfra 后续下掉
 	}, data, AppTokenMiddleware, TenantAndUserMiddleware, ServiceIDMiddleware)
 	if err != nil {
@@ -29,20 +21,8 @@ func SendLog(ctx context.Context, data interface{}) error {
 
 	code := gjson.GetBytes(body, "code").String()
 	msg := gjson.GetBytes(body, "msg").String()
-	if code == SuccessCode {
+	if code == exp.SCSuccess {
 		return nil
 	}
-
-	if IsSysError(code) {
-		return exp.InternalError("Send log failed, ErrCode: %s, ErrMsg: %s", code, msg)
-	}
-	return exp.InvalidParamError("Send log failed, ErrCode: %s, ErrMsg: %s", code, msg)
-}
-
-func IsSysError(errCode string) bool {
-	return errCode == FaaSInfraFailCodeInternalError ||
-		errCode == FaaSInfraFailCodeTokenExpire ||
-		errCode == FaaSInfraFailCodeIllegalToken ||
-		errCode == FaaSInfraFailCodeMissingToken ||
-		errCode == FaaSInfraFailCodeRateLimitError
+	return exp.NewErrWithCode(code, msg, utils.GetLogIDFromExtra(extra))
 }
--- http/innerapi.go
+++ http/innerapi.go
@@ -6,56 +6,12 @@ package http
 import (
 	"context"
 	"encoding/json"
-	"strconv"
-
 	"github.com/byted-apaas/server-common-go/constants"
 	cExceptions "github.com/byted-apaas/server-common-go/exceptions"
 	"github.com/byted-apaas/server-common-go/structs"
 	"github.com/byted-apaas/server-common-go/utils"
-	"github.com/tidwall/gjson"
+	"strconv"
 )
-
-const (
-	SCFileDownload = ""
-	SCSuccess      = "0"
-
-	ECInternalError  = "k_ec_000001"
-	ECNoTenantID     = "k_ec_000002"
-	ECNoUserID       = "k_ec_000003"
-	ECUnknownError   = "k_ec_000004"
-	ECOpUnknownError = "k_op_ec_00001"
-	ECSystemBusy     = "k_op_ec_20001"
-	ECSystemError    = "k_op_ec_20002"
-	ECRateLimitError = "k_op_ec_20003"
-	ECTokenExpire    = "k_ident_013000"
-	ECIllegalToken   = "k_ident_013001"
-	ECMissingToken   = "k_op_ec_10205"
-)
-
-func errorWrapper(body []byte, extra map[string]interface{}, err error) ([]byte, error) {
-	if err != nil {
-		return nil, cExceptions.ErrWrap(err)
-	}
-
-	code := gjson.GetBytes(body, "code").String()
-	msg := gjson.GetBytes(body, "msg").String()
-	switch code {
-	case SCFileDownload:
-		return body, nil
-	case SCSuccess:
-		data := gjson.GetBytes(body, "data")
-		if data.Type == gjson.String {
-			return []byte(data.Str), nil
-		}
-		return []byte(data.Raw), nil
-	case ECInternalError, ECNoTenantID, ECNoUserID, ECUnknownError,
-		ECOpUnknownError, ECSystemBusy, ECSystemError, ECRateLimitError,
-		ECTokenExpire, ECIllegalToken, ECMissingToken:
-		return nil, cExceptions.InternalError("%v ([%v] %v)", msg, code, utils.GetLogIDFromExtra(extra))
-	default:
-		return nil, cExceptions.InvalidParamError("%v ([%v] %v)", msg, code, utils.GetLogIDFromExtra(extra))
-	}
-}
 
 func GetAppTokenHttp(ctx context.Context, clientID, clientSecret string) (*structs.AppTokenResp, error) {
 	data := map[string]interface{}{
@@ -64,7 +20,7 @@ func GetAppTokenHttp(ctx context.Context, clientID, clientSecret string) (*struc
 		"withTenantInfo": true,
 	}
 
-	body, err := errorWrapper(GetOpenapiClient().PostJson(ctx, OpenapiPathGetToken, nil, data))
+	body, err := utils.ErrorWrapper(GetOpenapiClient().PostJson(ctx, OpenapiPathGetToken, nil, data))
 	if err != nil {
 		return nil, err
 	}
@@ -87,7 +43,7 @@ func GetFunctionMetaHttp(ctx context.Context, apiName string) (funcMeta *structs
 		return nil, cExceptions.ErrWrap(err)
 	}
 
-	data, err := errorWrapper(body, extra, err)
+	data, err := utils.ErrorWrapper(body, extra, err)
 	if err != nil {
 		return nil, err
 	}
