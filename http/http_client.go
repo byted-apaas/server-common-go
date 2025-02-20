db7ffa17dd8526cd1288ab1755535c0771b5464e
wangjinwu <wangjinwu@bytedance.com>
2025-02-20 12:14:54 +0800

    feat: 去掉日志

--- http/http_client.go
+++ http/http_client.go
@@ -135,7 +135,7 @@ func (c *HttpClient) doRequest(ctx context.Context, req *http.Request, headers m
 	quota := utils.GetPodRateLimitQuotaFromCtx(ctx)
 	oldQuota := limiter.maxRequest
 	if reset := limiter.ResetRateLimiter(quota); reset {
-		fmt.Println(fmt.Sprintf("%s rate limit reset from %d to %d, apiID: %s", utils.GetFormatDate(), oldQuota, quota, utils.GetFuncAPINameFromCtx(ctx)))
+		fmt.Println(fmt.Sprintf("%s rate limit reset from %d to %d, apiID: %s, tenantID: %d, namespace: %s", utils.GetFormatDate(), oldQuota, quota, utils.GetFuncAPINameFromCtx(ctx), utils.GetTenantIDFromCtx(ctx), utils.GetNamespaceFromCtx(ctx)))
 	}
 
 	if !limiter.AllowRequest() {
@@ -161,14 +161,10 @@ func (c *HttpClient) doRequest(ctx context.Context, req *http.Request, headers m
 		}
 		// 触发限流，禁止访问
 		if downgrade := utils.GetPodRateLimitDowngradeFromCtx(ctx); !downgrade {
-			fmt.Println(fmt.Sprintf("%s rate_limit function: %v limit exceeded quota: %d qps", utils.GetFormatDate(), utils.GetFunctionAPIIDFromCtx(ctx), quota))
 			return nil, nil, fmt.Errorf("request limit exceeded quota: %d qps", quota)
 		}
 		// 触发限流，降级通过
-		fmt.Println(fmt.Sprintf("%s rate_limit function: %v limit exceeded quota: %d qps, downgrade pass", utils.GetFormatDate(), utils.GetFunctionAPIIDFromCtx(ctx), quota))
 	}
-
-	fmt.Println(fmt.Sprintf("%s rate_limit function: %v do request quota: %d qps", utils.GetFormatDate(), utils.GetFunctionAPIIDFromCtx(ctx), quota))
 
 	extra := map[string]interface{}{}
 
