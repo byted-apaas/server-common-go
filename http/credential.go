642057af0208d3693bdf8a748aab07e9c2e441e9
xiaochuyu <xiaochuyu.xcy@bytedance.com>
2025-08-19 20:06:25 +0800

    feat: update

--- http/credential.go
+++ http/credential.go
@@ -71,7 +71,8 @@ func (c *AppCredential) GetTenantInfo(ctx context.Context) (*structs.Tenant, err
 	if ok {
 		return tenant, nil
 	}
-	ctx = withPressureSdkReqTag(ctx) // 添加反压标识，防止refresh中doRequest形成死锁
+	// fix: 修复进程启动后第一个请求为GetTenantInfo且触发反压时的死锁问题
+	ctx = withPressureSdkReqTag(ctx) // 添加反压标识，防止refresh.lock死锁
 	_, tenantInfo, err := c.refresh(ctx)
 	return tenantInfo, err
 }
